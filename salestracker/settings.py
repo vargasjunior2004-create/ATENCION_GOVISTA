1092d2ba6aa32fff5dd3f3e6a4c5289b04b6b1e3
Junior Villarroel <vargasjunior2004@gmail.com>
2026-08-21 22:17:05 -0400

    feat: JWT token lifetime 8h, refresh 24h

--- salestracker/settings.py
+++ salestracker/settings.py
@@ -123,8 +123,8 @@ REST_FRAMEWORK = {
 
 SIMPLE_JWT = {
     'AUTH_HEADER_TYPES': ('Bearer',),
-    'ACCESS_TOKEN_LIFETIME': __import__('datetime').timedelta(minutes=30),
-    'REFRESH_TOKEN_LIFETIME': __import__('datetime').timedelta(hours=2),
+    'ACCESS_TOKEN_LIFETIME': __import__('datetime').timedelta(hours=8),
+    'REFRESH_TOKEN_LIFETIME': __import__('datetime').timedelta(hours=24),
     'ROTATE_REFRESH_TOKENS': False,
 }
 
